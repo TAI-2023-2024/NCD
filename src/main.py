b107a5bd28627cd02eed2c1c0cde3b10035c1d74
Bruno-EMpy <73220067+Bruno-EMpy@users.noreply.github.com>
2024-06-09 17:11:40 +0100

    generate signatures and database

--- src/main.py
+++ src/main.py
@@ -5,6 +5,66 @@ import lzma
 import gzip
 import bz2
 from pprint import pp
+import subprocess
+
+# DECLARATIONS
+available_compressors=[
+        "zlib",
+        "lzma",
+        "gzip",
+        "bz2"
+    ]
+
+flags={
+    "Process" : 0, # 0: generate Database, 1:Classify input file
+    "Compressor": available_compressors[0]
+}
+
+data_path = os.path.realpath(__file__).replace("src\\"+os.path.basename(__file__),"Data\\")
+Database = "Database"
+temp = "temp"
+Signatures = "Signatures"
+progPATHS = {
+    "Database": data_path+Database,
+    "temp": data_path+temp,
+    "Signatures": data_path+Signatures
+}
+
+GetMaxFreqs = os.path.realpath(__file__).replace("src\\"+os.path.basename(__file__),"GetMaxFreqs\\bin\\GetMaxFreqs.exe")
+
+# Signatures
+def sig_file_name (wav_filename, p=temp):
+    return progPATHS[p] + "\\"+ os.path.basename(wav_filename) + ".sig"
+
+def getmaxfreqs_signatures(filename, p = temp):
+    if(str(filename).endswith(".wav") and os.path.exists(filename)):
+        sigFile = sig_file_name(filename,p)
+        cmd = [GetMaxFreqs, "-w", sigFile, os.path.realpath(filename)]
+        if(subprocess.run(cmd)==0):
+            return [0,sigFile]
+        else:
+            return [1,f"File not found. Please check if the file exists and if it is a WAV file. [FILE: {filename}]"]
+    else:
+        return [1,"Error"]
+
+def gen_database():
+    #generate signatures
+    ls=list(filter(lambda f : f.endswith(".wav"), list(os.listdir(progPATHS[Database]))))
+    if (len(ls)==0):
+        return [1,f"DATABASE: No WAV files found on {progPATHS[Database]}"]
+    
+    for file in ls:
+        ffile = progPATHS[Database]+"\\"+file
+        ffileSig = sig_file_name(progPATHS[Database]+"\\"+file,Signatures)
+        if (os.path.exists(ffileSig)):
+            os.remove(ffileSig)
+        getmaxfreqs_signatures(ffile,Signatures)
+
+    sigls = os.listdir(progPATHS["Signatures"])
+    if(len(sigls)!= len(ls)):
+        return [1,f"DATABASE: Signature files not generated for all Database files"]
+    else:
+        return [0,"OK"]
 
 # different functions C for NCD
 @ft.lru_cache()
@@ -40,12 +100,13 @@ def ncd(sample, train_sample, fn):
 
 
 def main():
-    algorithms = [
-        "zlib",
-        "lzma",
-        "gzip",
-        "bz2"
-    ]
+    # algorithms = [
+    #     "zlib",
+    #     "lzma",
+    #     "gzip",
+    #     "bz2"
+    # ]
+
 
     scores = list()
     # read signature file to predict
@@ -59,16 +120,24 @@ def main():
                 "file": file,
                 **{
                     alg: ncd(sample_to_predict, train_binary, globals()[f"compress_{alg}"])
-                    for alg in algorithms
+                    for alg in available_compressors
                 }
             })
 
     results = dict()
     # Find the smallest distance according to each algorithm
-    for alg in algorithms:
+    for alg in available_compressors:
         results[alg] = sorted(scores, key=lambda res: res[alg])[0]["file"]
     pp(results)
 
 
 if __name__ == "__main__":
-    main()
+    
+    
+
+    match flags["Process"]:
+        case 0:
+            gen_database()
+        case _,1:
+            main()
+            
